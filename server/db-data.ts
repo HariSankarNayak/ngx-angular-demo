cfa69bed3a5141d7dd3161c416c898e9ab1651c0
Hari Sankar Nayak <harisankar.nayak@nettantra.net>
2022-03-17 11:36:03 +0530

    Initial Commit

---
+++ server/db-data.ts
@@ -0,0 +1,630 @@
+export const USERS = {
+  1: {
+    id: 1,
+    email: 'test@angular-university.io',
+    password: 'test'
+  }
+
+};
+
+
+export const COURSES: any = {
+
+  4: {
+    id: 4,
+    description: 'Description for Title',
+    longDescription: 'Dumy Text for long description',
+    iconUrl: 'https://avatars.githubusercontent.com/u/60642121?v=4',
+    category: 'BEGINNER',
+    lessonsCount: 10,
+    seqNo: 0,
+    url: 'ngrx-course'
+  },
+
+  2: {
+    id: 2,
+    description: 'Description for Title',
+    longDescription: 'Dumy Text for long description',
+    iconUrl: 'https://avatars.githubusercontent.com/u/60642121?v=4',
+    lessonsCount: 10,
+    category: 'BEGINNER',
+    seqNo: 1,
+    url: 'angular-core-course'
+  },
+
+  3: {
+    id: 3,
+    description: 'Description for Title',
+    longDescription: 'Dumy Text for long description',
+    iconUrl: 'https://avatars.githubusercontent.com/u/60642121?v=4',
+    category: 'BEGINNER',
+    lessonsCount: 10,
+    seqNo: 2,
+    url: 'rxjs-course'
+  },
+
+  1: {
+    id: 1,
+    description: 'Description for Title',
+    longDescription: 'Dumy Text for long description',
+    iconUrl: 'https://avatars.githubusercontent.com/u/60642121?v=4',
+    lessonsCount: 10,
+    category: 'BEGINNER',
+    seqNo: 4,
+    url: 'serverless-angular'
+  },
+
+  /*
+
+
+  5: {
+    id: 5,
+    description: 'Description for Title',
+    longDescription: 'Dumy Text for long description',
+    iconUrl: 'https://avatars.githubusercontent.com/u/60642121?v=4',
+    category: 'BEGINNER',
+    lessonsCount: 10,
+    seqNo: 5,
+    url: 'angular-for-beginners'
+  },
+
+*/
+
+  12: {
+    id: 12,
+    description: 'Description for Title',
+    longDescription: 'Dumy Text for long description',
+    iconUrl: 'https://avatars.githubusercontent.com/u/60642121?v=4',
+    category: 'BEGINNER',
+    seqNo: 6,
+    url: 'angular-testing-course',
+    lessonsCount: 10,
+  },
+
+  6: {
+    id: 6,
+    description: 'Description for Title',
+    longDescription: 'Dumy Text for long description',
+    iconUrl: 'https://avatars.githubusercontent.com/u/60642121?v=4',
+    category: 'ADVANCED',
+    lessonsCount: 11,
+    seqNo: 7,
+    url: 'angular-security-course'
+  },
+
+  7: {
+    id: 7,
+    description: 'Description for Title',
+    longDescription: 'Dumy Text for long description',
+    iconUrl: 'https://avatars.githubusercontent.com/u/60642121?v=4',
+    category: 'ADVANCED',
+    lessonsCount: 8,
+    seqNo: 8,
+    url: 'angular-pwa-course'
+  },
+
+  8: {
+    id: 8,
+    description: 'Description for Title',
+    longDescription: 'Dumy Text for long description',
+    iconUrl: 'https://avatars.githubusercontent.com/u/60642121?v=4',
+    category: 'ADVANCED',
+    seqNo: 9,
+    url: 'angular-advanced-course'
+  },
+
+  9: {
+    id: 9,
+    description: 'Description for Title',
+    longDescription: 'Dumy Text for long description',
+    iconUrl: 'https://avatars.githubusercontent.com/u/60642121?v=4',
+    category: 'BEGINNER',
+    seqNo: 10,
+    url: 'typescript-course'
+  },
+
+  10: {
+    id: 10,
+    description: 'Description for Title',
+    longDescription: 'Dumy Text for long description',
+    iconUrl: 'https://avatars.githubusercontent.com/u/60642121?v=4',
+    category: 'F',
+    seqNo: 11,
+    url: 'rxjs-patterns-course'
+  },
+
+  11: {
+    id: 11,
+    description: 'Description for Title',
+    longDescription: 'Dumy Text for long description',
+    iconUrl: 'https://avatars.githubusercontent.com/u/60642121?v=4',
+    category: 'BEGINNER',
+    seqNo: 12,
+    url: 'angular-material-course'
+  }
+
+};
+
+
+export const LESSONS = {
+
+  1: {
+    id: 1,
+    'description': 'Description for Title Child',
+    'duration': '4:17',
+    'seqNo': 1,
+    courseId: 5
+  },
+  2: {
+    id: 2,
+    'description': 'Description for Title Child',
+    'duration': '2:07',
+    'seqNo': 2,
+    courseId: 5
+  },
+  3: {
+    id: 3,
+    'description': 'Description for Title Child',
+    'duration': '2:33',
+    'seqNo': 3,
+    courseId: 5
+  },
+  4: {
+    id: 4,
+    'description': 'Description for Title Child',
+    'duration': '4:44',
+    'seqNo': 4,
+    courseId: 5
+  },
+  5: {
+    id: 5,
+    'description': 'Description for Title Child',
+    'duration': '2:55',
+    'seqNo': 5,
+    courseId: 5
+  },
+  6: {
+    id: 6,
+    'description': 'Description for Title Child',
+    'duration': '3:27',
+    'seqNo': 6,
+    courseId: 5
+  },
+  7: {
+    id: 7,
+    'description': 'Description for Title Child',
+    'duration': '9:22',
+    'seqNo': 7,
+    courseId: 5
+  },
+  8: {
+    id: 8,
+    'description': 'Description for Title Child',
+    'duration': '1:26',
+    'seqNo': 8,
+    courseId: 5
+  },
+  9: {
+    id: 9,
+    'description': 'Description for Title Child',
+    'duration': '2:08',
+    'seqNo': 9,
+    courseId: 5
+  },
+  10: {
+    id: 10,
+    'description': 'Description for Title Child',
+    'duration': '4:01',
+    'seqNo': 10,
+    courseId: 5
+  },
+
+
+  // Security Course
+  11: {
+    id: 11,
+    'description': 'Description for Title Child',
+    'duration': '08:19',
+    'seqNo': 1,
+    courseId: 6
+  },
+
+  12: {
+    id: 12,
+    'description': 'Description for Title Child',
+    'duration': '04:17',
+    'seqNo': 2,
+    courseId: 6
+  },
+
+  13: {
+    id: 13,
+    'description': 'Description for Title Child',
+    'duration': '06:05',
+    'seqNo': 3,
+    courseId: 6
+  },
+
+  14: {
+    id: 14,
+    'description': 'Description for Title Child',
+    'duration': '03:57',
+    'seqNo': 4,
+    courseId: 6
+  },
+
+  15: {
+    id: 15,
+    'description': 'Description for Title Child',
+    'duration': '06:00',
+    'seqNo': 5,
+    courseId: 6
+  },
+  16: {
+    id: 16,
+    'description': 'Description for Title Child',
+    'duration': '04:53',
+    'seqNo': 6,
+    courseId: 6
+  },
+  17: {
+    id: 17,
+    'description': 'Description for Title Child',
+    'duration': '09:14',
+    'seqNo': 7,
+    courseId: 6
+  },
+  18: {
+    id: 18,
+    'description': 'Description for Title Child',
+    'duration': '06:08',
+    'seqNo': 8,
+    courseId: 6
+  },
+  19: {
+    id: 19,
+    'description': 'Description for Title Child',
+    'duration': '08:50',
+    'seqNo': 9,
+    courseId: 6
+  },
+  20: {
+    id: 20,
+    'description': 'Description for Title Child',
+    'duration': '05:46',
+    'seqNo': 10,
+    courseId: 6
+  },
+  21: {
+    id: 21,
+    'description': 'Description for Title Child',
+    'duration': '06:31',
+    'seqNo': 11,
+    courseId: 6
+  },
+
+
+  // PWA course
+
+  22: {
+    id: 22,
+    'description': 'Description for Title Child',
+    'duration': '07:19',
+    'seqNo': 1,
+    courseId: 7
+  },
+  23: {
+    id: 23,
+    'description': 'Description for Title Child',
+    'duration': '6:59',
+    'seqNo': 2,
+    courseId: 7
+  },
+  24: {
+    id: 24,
+    'description': 'Description for Title Child',
+    'duration': '7:28',
+    'seqNo': 3,
+    courseId: 7
+  },
+  25: {
+    id: 25,
+    'description': 'Description for Title Child',
+    'duration': '10:17',
+    'seqNo': 4,
+    courseId: 7
+  },
+
+  26: {
+    id: 26,
+    'description': 'Description for Title Child',
+    'duration': '09:50',
+    'seqNo': 5,
+    courseId: 7
+  },
+  27: {
+    id: 27,
+    'description': 'Description for Title Child',
+    'duration': '04:44',
+    'seqNo': 6,
+    courseId: 7
+  },
+  28: {
+    id: 28,
+    'description': 'Description for Title Child',
+    'duration': '06:07',
+    'seqNo': 7,
+    courseId: 7
+  },
+  29: {
+    id: 29,
+    'description': 'Description for Title Child',
+    'duration': '5:38',
+    'seqNo': 8,
+    courseId: 7
+  },
+
+  // Serverless Angular with Firebase Course
+
+  30: {
+    id: 30,
+    'description': 'Description for Title Child',
+    'duration': '5:38',
+    'seqNo': 1,
+    courseId: 1
+  },
+
+  31: {
+    id: 31,
+    'description': 'Description for Title Child',
+    'duration': '5:12',
+    'seqNo': 2,
+    courseId: 1
+  },
+
+  32: {
+    id: 32,
+    'description': 'Description for Title Child',
+    'duration': '4:07',
+    'seqNo': 3,
+    courseId: 1
+  },
+
+  33: {
+    id: 33,
+    'description': 'Description for Title Child',
+    'duration': '7:32',
+    'seqNo': 4,
+    courseId: 1
+  },
+
+  34: {
+    id: 34,
+    'description': 'Description for Title Child',
+    'duration': '6:28',
+    'seqNo': 5,
+    courseId: 1
+  },
+
+  35: {
+    id: 35,
+    'description': 'Description for Title Child',
+    'duration': '4:38',
+    'seqNo': 6,
+    courseId: 1
+  },
+
+  36: {
+    id: 36,
+    'description': 'Description for Title Child',
+    'duration': '7:54',
+    'seqNo': 7,
+    courseId: 1
+  },
+
+  37: {
+    id: 37,
+    'description': 'Description for Title Child',
+    'duration': '5:31',
+    'seqNo': 8,
+    courseId: 1
+  },
+
+  38: {
+    id: 38,
+    'description': 'Description for Title Child',
+    'duration': '8:19',
+    'seqNo': 9,
+    courseId: 1
+  },
+
+  39: {
+    id: 39,
+    'description': 'Description for Title Child',
+    'duration': '7:05',
+    'seqNo': 10,
+    courseId: 1
+  },
+
+
+  // Angular Testing Course
+
+  40: {
+    id: 40,
+    'description': 'Description for Title Child',
+    'duration': '5:38',
+    'seqNo': 1,
+    courseId: 12
+  },
+
+  41: {
+    id: 41,
+    'description': 'Description for Title Child',
+    'duration': '5:12',
+    'seqNo': 2,
+    courseId: 12
+  },
+
+  42: {
+    id: 42,
+    'description': 'Description for Title Child',
+    'duration': '4:07',
+    'seqNo': 3,
+    courseId: 12
+  },
+
+  43: {
+    id: 43,
+    'description': 'Description for Title Child',
+    'duration': '7:32',
+    'seqNo': 4,
+    courseId: 12
+  },
+
+  44: {
+    id: 44,
+    'description': 'Description for Title Child',
+    'duration': '6:28',
+    'seqNo': 5,
+    courseId: 12
+  },
+
+  45: {
+    id: 45,
+    'description': 'Description for Title Child',
+    'duration': '4:38',
+    'seqNo': 6,
+    courseId: 12
+  },
+
+  46: {
+    id: 46,
+    'description': 'Description for Title Child',
+    'duration': '7:54',
+    'seqNo': 7,
+    courseId: 12
+  },
+
+  47: {
+    id: 47,
+    'description': 'Description for Title Child',
+    'duration': '5:31',
+    'seqNo': 8,
+    courseId: 12
+  },
+
+  48: {
+    id: 48,
+    'description': 'Description for Title Child',
+    'duration': '8:19',
+    'seqNo': 9,
+    courseId: 12
+  },
+
+  49: {
+    id: 49,
+    'description': 'Description for Title Child',
+    'duration': '7:05',
+    'seqNo': 10,
+    courseId: 12
+  },
+
+
+  // Ngrx Course
+  50: {
+    id: 50,
+    'description': 'Description for Title Child',
+    "duration": "6:53",
+    "seqNo": 1,
+    courseId: 4
+
+  },
+  51: {
+    id: 51,
+    'description': 'Description for Title Child',
+    "duration": "5:52",
+    "seqNo": 2,
+    courseId: 4
+  },
+  52: {
+    id: 52,
+    'description': 'Description for Title Child',
+    "duration": "8:17",
+    "seqNo": 3,
+    courseId: 4
+  },
+  53: {
+    id: 53,
+    'description': 'Description for Title Child',
+    "duration": "7:47",
+    "seqNo": 4,
+    courseId: 4
+  },
+  54: {
+    id: 54,
+    'description': 'Description for Title Child',
+    "duration": "9:22",
+    "seqNo": 5,
+    courseId: 4
+  },
+  55: {
+    id: 55,
+    'description': 'Description for Title Child',
+    "duration": "7:07",
+    "seqNo": 6,
+    courseId: 4
+  },
+
+  56: {
+    id: 56,
+    'description': 'Description for Title Child',
+    "duration": "4:39",
+    "seqNo": 7,
+    courseId: 4
+  },
+  57: {
+    id: 57,
+    'description': 'Description for Title Child',
+    "duration": "4:44",
+    "seqNo": 8,
+    courseId: 4
+  },
+  58: {
+    id: 58,
+    'description': 'Description for Title Child',
+    "duration": "9:10",
+    "seqNo": 9,
+    courseId: 4
+  },
+  59: {
+    id: 59,
+    'description': 'Description for Title Child',
+    "duration": "9:10",
+    "seqNo": 10,
+    courseId: 4
+  }
+
+
+};
+
+
+export function findCourseById(courseId: number) {
+  return COURSES[courseId];
+}
+
+export function findLessonsForCourse(courseId: number) {
+  return Object.values(LESSONS).filter(lesson => lesson.courseId == courseId);
+}
+
+
+export function authenticate(email: string, password: string) {
+
+  const user: any = Object.values(USERS).find(user => user.email === email);
+
+  if (user && user.password == password) {
+    return user;
+  } else {
+    return undefined;
+  }
+
+}
